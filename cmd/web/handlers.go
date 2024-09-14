35e74b67816f77a002353beb7500e880388934e3
syfulsharif <sharif.syful@gmail.com>
2024-09-14 17:22:40 +0600

    Folder Structure Created

---
+++ cmd/web/handlers.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+// Define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a snippetView Handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	// Extract the value of the id parameter from the Query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return a 404 page
+	// not found response
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
+	// w.Write([]byte("Display a specific snippet"))
+}
+
+// Add a snippetCreate Handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check wheather the request is using POST or not.
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new snippet"))
+}
